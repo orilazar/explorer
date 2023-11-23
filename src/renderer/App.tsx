2bb786e627047bcee87fab229a07d943a48eb9aa
Ori Lazarovitch <ori101220011@gmail.com>
2023-11-23 21:51:06 +0200

    Added basic stuff

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,41 +1,12 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
-import icon from '../../assets/icon.svg';
 import './App.css';
+import { Box } from '@chakra-ui/react';
 
 function Hello() {
   return (
-    <div>
-      <div className="Hello">
-        <img width="200" alt="icon" src={icon} />
-      </div>
-      <h1>electron-react-boilerplate</h1>
-      <div className="Hello">
-        <a
-          href="https://electron-react-boilerplate.js.org/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              📚
-            </span>
-            Read our docs
-          </button>
-        </a>
-        <a
-          href="https://github.com/sponsors/electron-react-boilerplate"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="folded hands">
-              🙏
-            </span>
-            Donate
-          </button>
-        </a>
-      </div>
-    </div>
+    <Box position="fixed" right={0} bottom={0} width="20em" height="20em">
+      <h1>a</h1>
+    </Box>
   );
 }
 
