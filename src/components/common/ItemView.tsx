605d8108738859cb4c9d2cccb9f5be7d8d042f15
Ori Lazarovitch <ori101220011@gmail.com>
2023-11-26 15:48:24 +0200

    Added context menu

--- src/components/common/ItemView.tsx
+++ src/components/common/ItemView.tsx
@@ -1,7 +1,17 @@
-import { HStack, Icon, Td, Text, Tr } from '@chakra-ui/react';
+import {
+  HStack,
+  Icon,
+  MenuItem,
+  MenuList,
+  Td,
+  Text,
+  Tr,
+} from '@chakra-ui/react';
 import React from 'react';
 import { FileNodeModel } from '../../models/FileNodeModel';
 import { useExplorer } from '../../hooks/ItemsContext';
+import { ContextMenu } from 'chakra-ui-contextmenu';
+import { LuCopy } from 'react-icons/lu';
 
 interface ItemViewProps {
   item: FileNodeModel;
@@ -11,25 +21,53 @@ interface ItemViewProps {
 
 const ItemView: React.FC<ItemViewProps> = ({ item, icon }) => {
   const { searchItems, setSearchValue } = useExplorer();
+
   const onItemClick = () => {
     if (item.type === 'folder') {
       setSearchValue(item.fullPath);
       searchItems(item.fullPath);
     }
   };
-
+  // create a context menu for the item that has a copy action using chakra-ui-contextmenu
   return (
-    <Tr _hover={{ background: 'gray.700' }} onDoubleClick={onItemClick}>
-      <Td>
-        <HStack>
-          <Icon>{icon}</Icon>
-          <Text>{item.name}</Text>
-        </HStack>
-      </Td>
+    <ContextMenu<HTMLElement>
+      renderMenu={() => (
+        <MenuList>
+          <MenuItem
+            fontSize="lg"
+            icon={<LuCopy fontSize="1.2em" />}
+            onClick={() => navigator.clipboard.writeText(item.name)}
+          >
+            Copy Name
+          </MenuItem>
+          <MenuItem
+            fontSize="lg"
+            icon={<LuCopy fontSize="1.2em" />}
+            onClick={() => navigator.clipboard.writeText(item.fullPath)}
+          >
+            Copy Full path
+          </MenuItem>
+        </MenuList>
+      )}
+    >
+      {(ref: any) => (
+        <Tr
+          ref={ref}
+          _hover={{ background: 'gray.700' }}
+          onDoubleClick={onItemClick}
+        >
+          <Td>
+            <HStack>
+              <Icon>{icon}</Icon>
+              <Text>{item.name}</Text>
+            </HStack>
+          </Td>
 
-      <Td>{item.size}</Td>
-      <Td>{item.fullPath}</Td>
-    </Tr>
+          <Td>{item.size}</Td>
+          <Td>{item.fullPath}</Td>
+        </Tr>
+      )}
+    </ContextMenu>
   );
 };
 
